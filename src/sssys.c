c6ff9639f7b6ccb4b8fe59b001da411308b25335
matteodalboni <matteo.dalboni.20@gmail.com>
2025-03-22 16:14:53 +0100

    New coding style: using curly braces whenever possible to be clearer and avoid warnings with some compilers.

--- src/sssys.c
+++ src/sssys.c
@@ -21,17 +21,19 @@ int statespace_tf2ss(Statespace* ss, float* num, float* den)
 	const int Nx = ss->Nx;
 	float d0 = den[0], n0 = num[0] / d0;
 
-	if (ss->Nu != 1 || ss->Ny != 1) return 1;
+	if (ss->Nu != 1 || ss->Ny != 1) {
+		return 1;
+	}
 
-	for (i = 0; i < Nx; i++)
-	{
+	for (i = 0; i < Nx; i++) {
 		ss->A[i] = -den[i + 1] / d0;
 		ss->B[i] = !i;
 		ss->C[i] = num[i + 1] / d0 - n0 * den[i + 1] / d0;
 	}
 
-	for (i = Nx; i < Nx * Nx; i++)
+	for (i = Nx; i < Nx * Nx; i++) {
 		ss->A[i] = (float)((i % (Nx + 1)) == Nx);
+	}
 
 	ss->D[0] = n0;
 
@@ -67,11 +69,14 @@ int statespace_c2d(Statespace* ss, float Ts, char* method, float* work)
 		// Dd = D
 
 		// Ad = I+A*Ts
-		for (i = 0; i < Nxx; i++)
+		for (i = 0; i < Nxx; i++) {
 			A[i] = A[i] * Ts + !(i % (Nx + 1));
+		}
 
 		// Bd = B*Ts
-		for (i = 0; i < Nxu; i++) B[i] *= Ts;
+		for (i = 0; i < Nxu; i++) {
+			B[i] *= Ts;
+		}
 
 		break;
 
@@ -83,43 +88,59 @@ int statespace_c2d(Statespace* ss, float Ts, char* method, float* work)
 		// Dd = C*(I-A*Ts)^(-1)*B*Ts+D = C*Bd+D
 
 		// R = [I, B*Ts]
-		for (k = j = 0; j < Nx; j++)
-			for (i = 0; i < Nx; i++)
+		for (k = j = 0; j < Nx; j++) {
+			for (i = 0; i < Nx; i++) {
 				work[k++] = (float)(i == j);
-		for (j = 0; j < Nu; j++)
-			for (i = 0; i < Nx; i++)
+			}
+		}
+		for (j = 0; j < Nu; j++) {
+			for (i = 0; i < Nx; i++) {
 				work[k++] = B[i * Nu + j] * Ts;
+			}
+		}
 		matrixf_init(&R, Nx, Nx + Nu, work, 0);
 
 		// L = I-A*Ts
-		for (i = 0; i < Nxx; i++)
+		for (i = 0; i < Nxx; i++) {
 			A[i] = A[i] * (-1.0f) * Ts + !(i % (Nx + 1));
+		}
 		matrixf_init(&L, Nx, Nx, A, 1);
 
 		// Solve L*X = R for X, where X = [Ad, Bd]
-		if (matrixf_solve_lu(&L, &R)) return 1;
+		if (matrixf_solve_lu(&L, &R)) {
+			return 1;
+		}
 		matrixf_init(&Ad, Nx, Nx, work, 0);
 		matrixf_init(&Bd, Nx, Nu, work + Nxx, 0);
 		matrixf_transpose(&Ad);
 		matrixf_transpose(&Bd);
-		for (i = 0; i < Nxx; i++) A[i] = Ad.data[i];
-		for (i = 0; i < Nxu; i++) B[i] = Bd.data[i];
+		for (i = 0; i < Nxx; i++) {
+			A[i] = Ad.data[i];
+		}
+		for (i = 0; i < Nxu; i++) {
+			B[i] = Bd.data[i];
+		}
 
 		// Dd = C*Bd+D
-		for (i = 0; i < Ny; i++)
-			for (j = 0; j < Nu; j++)
-				for (k = 0; k < Nx; k++)
+		for (i = 0; i < Ny; i++) {
+			for (j = 0; j < Nu; j++) {
+				for (k = 0; k < Nx; k++) {
 					D[i * Nu + j] += C[i * Nx + k] * B[k * Nu + j];
+				}
+			}
+		}
 
 		// Cd = C*Ad
 		for (i = 0; i < Ny; i++) {
 			for (j = 0; j < Nx; j++) {
 				work[j + Nxx] = 0;
-				for (k = 0; k < Nx; k++)
+				for (k = 0; k < Nx; k++) {
 					work[j + Nxx] += C[i * Nx + k] * Ad.data[k * Nx + j];
+				}
 			}
-			for (j = 0; j < Nx; j++)
+			for (j = 0; j < Nx; j++) {
 				C[i * Nx + j] = work[j + Nxx];
+			}
 		}
 
 		break;
@@ -132,48 +153,66 @@ int statespace_c2d(Statespace* ss, float Ts, char* method, float* work)
 		// Dd = C*(I-A*Ts/2)^(-1)*B*Ts/2+D = C*Bd/2+D
 
 		// R = [I, A*Ts/2, B*Ts]
-		for (k = j = 0; j < Nx; j++)
-			for (i = 0; i < Nx; i++)
+		for (k = j = 0; j < Nx; j++) {
+			for (i = 0; i < Nx; i++) {
 				work[k++] = (float)(i == j);
-		for (j = 0; j < Nx; j++)
-			for (i = 0; i < Nx; i++)
+			}
+		}
+		for (j = 0; j < Nx; j++) {
+			for (i = 0; i < Nx; i++) {
 				work[k++] = A[i * Nx + j] * 0.5f * Ts;
-		for (j = 0; j < Nu; j++)
-			for (i = 0; i < Nx; i++)
+			}
+		}
+		for (j = 0; j < Nu; j++) {
+			for (i = 0; i < Nx; i++) {
 				work[k++] = B[i * Nu + j] * Ts;
+			}
+		}
 		matrixf_init(&R, Nx, Nx + Nx + Nu, work, 0);
 
 		// L = I-A*Ts/2
-		for (i = 0; i < Nxx; i++)
+		for (i = 0; i < Nxx; i++) {
 			A[i] = A[i] * (-0.5f) * Ts + !(i % (Nx + 1));
+		}
 		matrixf_init(&L, Nx, Nx, A, 1);
 
 		// Solve L*X = R for X, where X = [Ad1, Ad2, Bd] and Ad = Ad1 + Ad2
-		if (matrixf_solve_lu(&L, &R)) return 1;
+		if (matrixf_solve_lu(&L, &R)) {
+			return 1;
+		}
 		matrixf_init(&Ad1, Nx, Nx, work, 0);
 		matrixf_init(&Ad2, Nx, Nx, work + Nxx, 0);
 		matrixf_init(&Bd, Nx, Nu, work + Nxx + Nxx, 0);
 		matrixf_transpose(&Ad1);
 		matrixf_transpose(&Ad2);
 		matrixf_transpose(&Bd);
-		for (i = 0; i < Nxx; i++) A[i] = Ad1.data[i] + Ad2.data[i];
-		for (i = 0; i < Nxu; i++) B[i] = Bd.data[i];
+		for (i = 0; i < Nxx; i++) {
+			A[i] = Ad1.data[i] + Ad2.data[i];
+		}
+		for (i = 0; i < Nxu; i++) {
+			B[i] = Bd.data[i];
+		}
 
 		// Dd = C*Bd/2+D
-		for (i = 0; i < Ny; i++)
-			for (j = 0; j < Nu; j++)
-				for (k = 0; k < Nx; k++)
+		for (i = 0; i < Ny; i++) {
+			for (j = 0; j < Nu; j++) {
+				for (k = 0; k < Nx; k++) {
 					D[i * Nu + j] += C[i * Nx + k] * B[k * Nu + j] * 0.5f;
+				}
+			}
+		}
 
 		// Cd = C*Ad1
 		for (i = 0; i < Ny; i++) {
 			for (j = 0; j < Nx; j++) {
 				work[j + Nxx] = 0;
-				for (k = 0; k < Nx; k++)
+				for (k = 0; k < Nx; k++) {
 					work[j + Nxx] += C[i * Nx + k] * Ad1.data[k * Nx + j];
+				}
 			}
-			for (j = 0; j < Nx; j++)
+			for (j = 0; j < Nx; j++) {
 				C[i * Nx + j] = work[j + Nxx];
+			}
 		}
 
 		break;
@@ -192,19 +231,29 @@ int statespace_c2d(Statespace* ss, float Ts, char* method, float* work)
 		matrixf_init(&L, Nx + Nu, Nx + Nu, work, 0);
 		for (i = 0; i < L.size[0]; i++) {
 			for (j = 0; j < L.size[1]; j++) {
-				if (j < Nx) at(&L, i, j) = (i < Nx) ? A[i * Nx + j] * Ts : 0;
-				else at(&L, i, j) = (i < Nx) ? B[i * Nu + j - Nx] * Ts : 0;
+				if (j < Nx) {
+					at(&L, i, j) = (i < Nx) ? A[i * Nx + j] * Ts : 0;
+				}
+				else {
+					at(&L, i, j) = (i < Nx) ? B[i * Nu + j - Nx] * Ts : 0;
+				}
 			}
 		}
 
 		// Compute exp(L) = [Ad, Bd; 0, I]
-		if (matrixf_exp(&L, work + L.size[0] * L.size[1])) return 1;
+		if (matrixf_exp(&L, work + L.size[0] * L.size[1])) {
+			return 1;
+		}
 
 		// Get the blocks Ad and Bd
 		for (i = 0; i < Nx; i++) {
 			for (j = 0; j < L.size[1]; j++) {
-				if (j < Nx) A[i * Nx + j] = at(&L, i, j);
-				else B[i * Nu + j - Nx] = at(&L, i, j);
+				if (j < Nx) {
+					A[i * Nx + j] = at(&L, i, j);
+				}
+				else {
+					B[i * Nu + j - Nx] = at(&L, i, j);
+				}
 			}
 		}
 
@@ -219,25 +268,41 @@ int statespace_c2d(Statespace* ss, float Ts, char* method, float* work)
 
 		// Assemble L = [A*Ts, B*Ts, 0; 0, 0, I; 0, 0, 0]
 		matrixf_init(&L, Nx + 2 * Nu, Nx + 2 * Nu, work, 0);
-		for (i = 0; i < L.size[0] * L.size[1]; i++) work[i] = 0;
-		for (i = 0; i < Nu; i++) at(&L, Nx + i, Nx + Nu + i) = 1;
+		for (i = 0; i < L.size[0] * L.size[1]; i++) {
+			work[i] = 0;
+		}
+		for (i = 0; i < Nu; i++) {
+			at(&L, Nx + i, Nx + Nu + i) = 1;
+		}
 		for (i = 0; i < Nx; i++) {
 			for (j = 0; j < Nx + Nu; j++) {
-				if (j < Nx) at(&L, i, j) = A[i * Nx + j] * Ts;
-				else at(&L, i, j) = B[i * Nu + j - Nx] * Ts;
+				if (j < Nx) {
+					at(&L, i, j) = A[i * Nx + j] * Ts;
+				}
+				else {
+					at(&L, i, j) = B[i * Nu + j - Nx] * Ts;
+				}
 			}
 		}
 
 		// Compute exp(L) = [Ad, G1, G2; 0, I, I; 0, 0, I]
 		p = L.size[0] * L.size[1];
-		if (matrixf_exp(&L, work + p)) return 1;
+		if (matrixf_exp(&L, work + p)) {
+			return 1;
+		}
 
 		// Get the blocks Ad, G1 and G2
 		for (i = 0; i < Nx; i++) {
 			for (j = 0; j < L.size[1]; j++) {
-				if (j < Nx) A[i * Nx + j] = at(&L, i, j);
-				else if (j < Nx + Nu) B[i * Nu + j - Nx] = at(&L, i, j);
-				else work[i * Nu + j - Nx - Nu + p] = at(&L, i, j);
+				if (j < Nx) {
+					A[i * Nx + j] = at(&L, i, j);
+				}
+				else if (j < Nx + Nu) {
+					B[i * Nu + j - Nx] = at(&L, i, j);
+				}
+				else {
+					work[i * Nu + j - Nx - Nu + p] = at(&L, i, j);
+				}
 			}
 		}
 
@@ -245,16 +310,20 @@ int statespace_c2d(Statespace* ss, float Ts, char* method, float* work)
 		for (i = 0; i < Nx; i++) {
 			for (j = 0; j < Nu; j++) {
 				B[i * Nu + j] -= work[i * Nu + j + p];
-				for (k = 0; k < Nx; k++)
+				for (k = 0; k < Nx; k++) {
 					B[i * Nu + j] += A[i * Nx + k] * work[k * Nu + j + p];
+				}
 			}
 		}
 
 		// Dd = D+C*G2
-		for (i = 0; i < Ny; i++)
-			for (j = 0; j < Nu; j++)
-				for (k = 0; k < Nx; k++)
+		for (i = 0; i < Ny; i++) {
+			for (j = 0; j < Nu; j++) {
+				for (k = 0; k < Nx; k++) {
 					D[i * Nu + j] += C[i * Nx + k] * work[k * Nu + j + p];
+				}
+			}
+		}
 
 		break;
 	}
@@ -274,25 +343,31 @@ void statespace_iter(Statespace* ss, float* u, float* work)
 	float* D = ss->D;
 	float tmp;
 
-	for (i = 0; i < Nx; i++) work[i] = ss->x[i];
+	for (i = 0; i < Nx; i++) {
+		work[i] = ss->x[i];
+	}
 
 	// y[k] = Cd*x[k] + Dd*u[k]
 	for (i = 0; i < Ny; i++) {
 		tmp = 0;
-		for (j = 0; j < Nx; j++)
+		for (j = 0; j < Nx; j++) {
 			tmp += C[i * Nx + j] * work[j];
-		for (j = 0; j < Nu; j++)
+		}
+		for (j = 0; j < Nu; j++) {
 			tmp += D[i * Nu + j] * u[j];
+		}
 		ss->y[i] = tmp;
 	}
 
 	// x[k+1] = Ad*x[k] + Bd*u[k]
 	for (i = 0; i < Nx; i++) {
 		tmp = 0;
-		for (j = 0; j < Nx; j++)
+		for (j = 0; j < Nx; j++) {
 			tmp += A[i * Nx + j] * work[j];
-		for (j = 0; j < Nu; j++)
+		}
+		for (j = 0; j < Nu; j++) {
 			tmp += B[i * Nu + j] * u[j];
+		}
 		ss->x[i] = tmp;
 	}
 }
